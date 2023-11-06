420101f1665fdc8bf964b0c548b102e9010d970f
pysatellite <pysatellite@gmail.com>
2023-11-06 17:51:19 +0900

    AttributeError: 'FieldInfo' object has no attribute 'name'
    - https://github.com/awtkns/fastapi-crudrouter/issues/190

--- src/fcrud/main.py
+++ src/fcrud/main.py
@@ -2,14 +2,24 @@ from typing import Union
 
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+from fastapi_crudrouter import MemoryCRUDRouter as CRUDRouter
+
+class Potato(BaseModel):
+    id: int
+    name: str
+    color: str
+    mass: float
+
 app = FastAPI()
+app.include_router(CRUDRouter(schema=Potato))
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
